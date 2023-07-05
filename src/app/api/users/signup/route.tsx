e8912578aeb1f89445cca7750873bf4f99e39870
Prakash Timilsina <prakashtimilsina84@gmail.com>
2023-07-05 10:41:46 -0500

    signup post api updated

--- src/app/api/users/signup/route.tsx
+++ src/app/api/users/signup/route.tsx
@@ -10,7 +10,36 @@ export async function POST(request: NextRequest){
     try {
       const reqBody =  await request.json();
       const {username, email, password} = reqBody;
-      
+
+      console.log(reqBody); //not recommended in Prod code
+
+      //Check if user already exists
+      const user = await User.findOne({email});
+
+      if (user){
+        return NextResponse.json({error: "User already exists"}, {status: 200});
+      }
+
+      //Hashing the password
+      const salt = await bcryptjs.genSalt(10);
+      const hashedPassword = await bcryptjs.hash(password, salt);
+
+      //Creating new User
+      const newUser = new User({
+        username,
+        email,
+        password : hashedPassword
+      })
+
+      const savedUser = await newUser.save();
+      console.log(savedUser);
+
+      return NextResponse.json({
+        message: "User created successfully",
+        success: true,
+        savedUser,
+      })
+
     } catch (error: any) {
         return NextResponse.json({error: error.message},{status: 500})
     }
