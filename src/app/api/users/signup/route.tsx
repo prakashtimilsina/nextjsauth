ab519469e1685c24c4088570784d60bccef5b942
Prakash Timilsina <prakashtimilsina84@gmail.com>
2023-07-05 10:32:23 -0500

    signup post api created

---
+++ src/app/api/users/signup/route.tsx
@@ -0,0 +1,17 @@
+import {connect} from '@/dbConfig/dbconfig';
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
+
+
+connect();
+
+export async function POST(request: NextRequest){
+    try {
+      const reqBody =  await request.json();
+      const {username, email, password} = reqBody;
+      
+    } catch (error: any) {
+        return NextResponse.json({error: error.message},{status: 500})
+    }
+}
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,8 +1,35 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
-
+    username :{
+        type: String,
+        required: [true, 'Please provide a username'],
+        unique: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, "Provide provide a password"]
+    },
+    isVerified:{
+        type: Boolean,
+        default: false,
+    },
+    isAdmin:{           //Better approach to go with roles instead of such isAdmin
+        type:Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
 })
 
-const User = mongoose.models.users || mongoose.model("User", userSchema);
+const User = mongoose.models.users || mongoose.model("User", userSchema); // MongoDB create database as users even if we provide the name as "User" or "user"
+
 
 export default User;
